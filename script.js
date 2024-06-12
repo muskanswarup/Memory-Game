4110113575f648f4bc9f7ded9a3a85e4fe8bfb53
Muskan Swarup <muskanswarup680@gmail.com>
2024-06-12 23:30:58 +0530

    added toggle

--- script.js
+++ script.js
@@ -3,65 +3,60 @@ const backFaceCards = document.querySelectorAll(".back-face");
 const cards = document.querySelectorAll(".card");
 const tiles = document.querySelectorAll(".tile");
 
-// /* I need to add this functiona;oty
-// .back-face {
-//     background-color: #f8f8f8;
-//     //  transform: rotateY(180deg);  
-// }
-// .tile:hover .card {
-//     transform: rotateY(180deg);
-// }
-// */
-
-// // for(let card in cards){
-// //     card.addEventListener("click" , () => {
-// //         console.log("YOu clicked a card");
-// //     } )
-// // }
-
-// function toggleCard (){
-//     let fCard = false;
-
-//     backFaceCards.forEach(bc => {
-//         bc.style.display = "none";
-
-//         fCard = true;
-//     })
-
-// }
-
 console.log("YOu working?");
 
-// function toggleCard(card){
-//     card.classList.toggle("is-flipped");
-// }
+let previousCardName = null;
 
+let clickedCards = [];
 
-
-// cards.forEach((card) => {
-//     card.addEventListener("click" , () => {
-
-//         let firstChild = card.firstElementChild;
-
-//         let cardName = firstChild.getAttribute("alt");
-//         console.log("YOu clicked me " , cardName);
-
-//         toggleCard(card);
-        
-//     })
-// })
-
-
-
-// const cards = document.querySelectorAll(".card");
 
 function toggleCard(card) {
-    card.classList.toggle("is-flipped");
+  let firstChild = card.firstElementChild;
+  let lastChild = card.lastElementChild;
+  let name;
+
+  if (firstChild.style.display === "block") {
+    firstChild.style.display = "none";
+    lastChild.style.display = "block";
+
+    name = lastChild.getAttribute("alt");
+  } else {
+    firstChild.style.display = "block";
+    lastChild.style.display = "none";
+
+    name = firstChild.getAttribute("alt");
+  }
+  return name;
+
 }
 
 cards.forEach((card) => {
-    card.addEventListener("click", () => {
-        toggleCard(card);
-    });
-});
+  card.addEventListener("click", () => {
 
+    if(clickedCards.length === 2){
+        clickedCards.forEach((clickedCard) => {
+            toggleCard(clickedCard);
+        });
+
+        clickedCards = [];
+    }
+
+    let firstChild = card.firstElementChild;
+
+    let cardName = firstChild.getAttribute("alt");
+    console.log("YOu clicked me", cardName);
+
+    toggleCard(card);
+
+    clickedCards.push(card);
+
+    // let nextCardName = toggleCard(card);
+    // console.log("Next card name : " + nextCardName);
+
+    // if (previousCardName !== null && nextCardName === previousCardName) {
+    //   console.log("point");
+    // }
+
+    // previousCardName = nextCardName;
+  });
+});
